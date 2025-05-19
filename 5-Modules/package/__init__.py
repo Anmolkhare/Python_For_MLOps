615adb7f87fb8378e757f3e868dcd63b5e959611
Anmolkhare <anmolkhare803@gmail.com>
2025-05-20 01:06:35 +0530

    standardlib

---
+++ 5-Modules/package/__init__.py
@@ -0,0 +1,1 @@
+# this file is used to define packages and initialize their namespaces
---
+++ 5-Modules/test.py
@@ -0,0 +1,9 @@
+from package.maths import *
+from package.subpackages.mult import multiply
+
+print(addition(5,5))
+print(substraction(5,5))
+
+print(multiply(5,5))
+
+# to run cd 5-Modules
